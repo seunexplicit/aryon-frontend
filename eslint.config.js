8497e3b2e620490290d3631a3ebda7c20b2a4d9a
seunexplicit <seunxplicit@gmail.com>
2024-11-28 20:59:30 +0100

    chore: module export

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,7 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
-export default tseslint.config(
+module.exports =  tseslint.config(
   { ignores: ['dist'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import path from 'path';
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
+module.exports = defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
